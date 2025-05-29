a8c69ae138599ef2939ff835437f14a50b409669
samirber22 <samirbertel@gmail.com>
2025-05-29 14:30:03 -0500

    Update Authentication System

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from .models import Usuario
 
@@ -26,6 +26,10 @@ class RegistroFormPersonalizado(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'Nombre de usuario'})
     )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Correo electrónico'})
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'})
     )
@@ -35,9 +39,13 @@ class RegistroFormPersonalizado(UserCreationForm):
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'first_name', 'last_name', 'fecha_nacimiento', 'telefono', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'fecha_nacimiento', 'telefono', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.help_text = ''
+
+class LoginFormPersonalizado(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nombre de usuario'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'}))
