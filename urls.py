0130d1a48c7a864ca027685590bdab761824dd36
samirber22 <samirbertel@gmail.com>
2025-05-29 14:30:38 -0500

    Update Authentication System

--- urls.py
+++ urls.py
@@ -6,11 +6,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 import os
 from django.contrib.auth.views import LogoutView
+from usuarios.forms import LoginFormPersonalizado
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='inicio'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html',
+        authentication_form=LoginFormPersonalizado
+    ), name='login'),
     path('logout/', LogoutView.as_view(next_page='inicio'), name='logout'),
     path('register/', views.register, name='register'),
     path('rutas/', views.rutas, name='rutas'),
@@ -18,5 +22,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'public'))
-
-
